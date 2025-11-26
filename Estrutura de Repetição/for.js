89acacacd924d8da71d3749850b0a0bcd8aff0fb
BARBARAH CAREY SANTOS OLIVEIRA <54411865858@imaccommand004.local>
2025-11-26 16:49:20 -0300

    exerciciofor

---
+++ Estrutura de Repetição/for.js
@@ -0,0 +1,20 @@
+//sintaxe = escrita do código
+
+// o for é um laço de repetição
+// um laço de repetição tem voltas - loop
+
+//laço contado - quando sabemos quantas voltas
+//laço condicional - irá fazer as voltas se a condição for verdadeira
+
+// for([inicialização]; [condição]; [incremento];){
+
+// }
+//a condic1ão verifica o número que o loop está
+for(let i = 0; i <= 9; i = i + 1){
+    console.log("A volta que for está é: " + i);
+    
+}
+
+//Utilize o for para exibir do 1 ao 10
+
+for(let i = 1; i <= 10; i++)
