1ca065e9313107e7cbb457d1acaa277a7ea8c892
BARBARAH CAREY SANTOS OLIVEIRA <54411865858@imaccommand004.local>
2025-11-26 09:39:23 -0300

    exercicio05

--- exercicio05.js
+++ exercicio05.js
@@ -1,6 +1,6 @@
-let n1 = Number ( prompt(" Digite o primeiro número da sua sequência"));
-let n2 = Number ( prompt(" Digite o segundo número da sua sequência"));
-let n3 = Number ( prompt(" Digite o terceiro número da sua sequência"));
+let n1 = Number ( prompt(" Digite o primeiro número"));
+let n2 = Number ( prompt(" Digite o segundo número"));
+let n3 = Number ( prompt(" Digite o terceiro número"));
 
 
 if ( n1 < n2 && n2 < n3 ) {
