3de565099332159d09a516cd64d77d6c8b967dc3
BARBARAH CAREY SANTOS OLIVEIRA <54411865858@imaccommand004.local>
2025-11-28 14:55:18 -0300

    funcao

---
+++ Função/funcao.js
@@ -0,0 +1,37 @@
+function exibirTabuada(){
+    let numero = parseInt(prompt("Digite um número para ver a tabuada:"));
+
+
+console.log (`====TABUADA ${numero}====`)
+
+for (let i = 1; i <= 10; i++) {
+    console.log(numero + "x" + i + "=" + (numero * i ));
+}
+
+}
+
+exibirTabuada();
+
+function exibirTabuada(){
+    let nomesolicitado = prompt("digite seu nome ");
+
+return nomesolicitado;
+}
+
+function exibirnomesolicitado(){
+    
+}
+
+// let nome = exibirTabuada();
+
+// alert(nome)
+
+function somaDoisNumeros(numero1, numero2){
+
+    console.log(numero1+numero2);
+}
+
+somaDoisNumeros(10,10);
+
+//Crie função para o exercicio 08 de Estrutura Condicional.
+//Passe via parametro o peso e a altura.
