95bc120e9c6f323f7ebc15a409fdc4c62d32b39c
BARBARAH CAREY SANTOS OLIVEIRA <54411865858@imaccommand004.local>
2025-11-28 15:05:31 -0300

    funcoespeso

--- Função/funcao.js
+++ Função/funcao.js
@@ -35,3 +35,32 @@ somaDoisNumeros(10,10);
 
 //Crie função para o exercicio 08 de Estrutura Condicional.
 //Passe via parametro o peso e a altura.
+
+
+function calcularIMC (peso, altura ){
+   
+
+let imc = peso / (altura * altura);
+
+let classificacao = "";
+
+if (imc < 18.5) {
+    classificacao = "Abaixo do peso";
+} else if (imc >= 18.5 && imc <= 24.9) {
+    classificacao = "Peso normal";
+} else if (imc >= 25.0 && imc <= 29.9) {
+    classificacao = "Sobrepeso";
+} else if (imc >= 30.0 && imc <= 34.9) {
+    classificacao = "Obesidade grau 1";
+} else if (imc >= 35.0 && imc <= 39.9) {
+    classificacao = "Obesidade grau 2";
+} else {
+    classificacao = "Obesidade grau 3";
+}
+
+alert("Obesidade grau 3. Se cuide por favor, sua vida estáem risco🤰");
+} 
+let pesoDigitado = parseFloat(prompt("Digite seu peso em kg:"));
+let alturaDigitado = parseFloat(prompt("Digite sua altura em metros:"));
+
+calcularIMC(pesoDigitado, alturaDigitado);
