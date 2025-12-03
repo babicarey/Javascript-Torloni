6239d7c4aa3a7338d3bb03486abdaae215a296f6
BARBARAH CAREY SANTOS OLIVEIRA <54411865858@imaccommand004.local>
2025-12-03 11:39:18 -0300

    document

--- dom/dom.js
+++ dom/dom.js
@@ -4,5 +4,33 @@ console.log(elementbody);
 
 let elementH1 = document.getElementsByTagName("h1"); 
 
-console.log(elementH1);
+elementH1[0].textContent= "Babi";
+// console.log(elementH1)
+ 
+
+
+let tecnicaLeandra = document.getElementById("tecnica");
+console.log(tecnicaLeandra);
+
+let jogadores = document.getElementsByClassName("jogador");
+
+console.log(jogadores)
+
+//-------------exibe po item 4 dos elementos que possuem a class jogador-------------
+let quatroJogadores = document.getElementsByClassName("jogador");
+
+console.log(quatroJogadores[4].textContent);
+
+
+let jogadorMuralha = document.getElementsByClassName("jogador");
+console.log(jogadorMuralha);
+console.log(jogadorMuralha[3]);
+jogadorMuralha[3].textContent = "bolacha";
+jogadorMuralha[3].style.color = "blue";
+
+
+tecnicaLeandra.style.color = "red";
+tecnicaLeandra.style.fontSize = "50px";
+
+
 
